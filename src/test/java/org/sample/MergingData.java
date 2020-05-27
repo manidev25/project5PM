7331739b3989b516d0ef9568dd9a04d39916f74e
manidev25 <manidevanmca@yahoo.com>
2020-05-27 14:50:35 +0530

    code updated

--- src/test/java/org/sample/MergingData.java
+++ src/test/java/org/sample/MergingData.java
@@ -2,8 +2,9 @@ package org.sample;
 
 public class MergingData {
 	public static void main(String[] args) {
-		System.out.println("hi");
 		System.out.println("Hello");
+		System.out.println("hi");
+	
 	}
 
 }
